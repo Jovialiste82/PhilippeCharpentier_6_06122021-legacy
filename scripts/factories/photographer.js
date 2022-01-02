b07a5f795db9ce884b2317a5f2d30e790f9cf4d6
Jovialiste82 <52076767+Jovialiste82@users.noreply.github.com>
2022-01-02 20:20:54 +0000

    gh page display bug

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -24,7 +24,7 @@ function photographerFactory(data) {
 
   function getUserCardDOM() {
     const article = document.createElement("article");
-    const baseURL = "PhilippeCharpentier_6_06122021/photographer.html";
+    const baseURL = "photographer.html";
     const url = `${baseURL}?photographer=${id}`;
     article.innerHTML = `
       <a href=${url}>
